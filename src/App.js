60810c343cefae839e6c8535be0165670635e166
NAFIZ009 <jalalahmednafiz@gmail.com>
2023-01-20 19:07:45 +0600

    mid

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Main from './Pages/Main';
 
 function App() {
   return (
-    <div className="App  bg-purple-200">
+    <div className="App  bg-purple-200 py-20">
           <Main></Main>
     </div>
   );
--- src/Component/Card.js
+++ src/Component/Card.js
@@ -1,7 +1,21 @@
 import React from 'react';
+import { useRef } from 'react';
 
 const Card = ({data}) => {
     const {name,contact,city,state,discription,address,designation,email,phone,country}=data;
+    const ref=useRef();
+
+    const btnFun=e=>{
+            if(ref.current.classList.contains("hidden")){
+                e.target.innerHTML="Hide Details";
+                ref.current.classList.remove("hidden");
+            }else{
+                e.target.innerHTML="View Details";
+                ref.current.classList.add("hidden");
+            }
+            
+    }
+
     return (
         <div>
         <div className="card bg-base-100 shadow-xl shadow mx-5 my-3 lg:p-10">
@@ -39,8 +53,8 @@ const Card = ({data}) => {
                         </p>
                     </div>
 
-                    <div className='py-5 lg:order-last w-full'>
-                            <div className="py-5 px-3 text-left gap-6 flex flex-col justify-between align-center border-2 border-gray-200 rounded-lg" style={{textTransform:"capitalize"}}>
+                    <div className='py-5 lg:order-last w-full border-2 border-gray-200 rounded-lg hidden' ref={ref} >
+                            <div className="py-5 px-3 text-left gap-6 flex flex-col justify-between align-center " style={{textTransform:"capitalize"}}>
                                 <div>
                                     <div className='flex flex-col gap-1'>
                                     <h1 className='text-lg font-bold text-gray-500'>
@@ -105,13 +119,10 @@ const Card = ({data}) => {
                                     </div>
                                 
                                 </div>
-                                
-            
-
                             </div>
-                        </div>
+                    </div>
             <div className="card-actions justify-center">
-                <button className="btn bg-purple-700 border-none rounded-lg capitalize btn-md hover:scale-105 hover:bg-purple-800">View Details</button>
+                <button className="btn bg-purple-700 border-none rounded-lg capitalize btn-md hover:scale-105 hover:bg-purple-800" onClick={btnFun}>View Details</button>
             </div>
             </div>
         </div>
